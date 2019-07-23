102c7207303a25d38448b7c16abd82699079488a
周码云 <5162617+qijichengzhen@user.noreply.gitee.com>
2019-07-23 14:46:39 +0800

    第一次上传

---
+++ git-test/git/src/com/zhoujia/git.java
@@ -0,0 +1,12 @@
+package com.zhoujia;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+}
